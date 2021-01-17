fb7cc31a558629f8abbf978655317cb1181587b4
Juliana Cavalaro <juliana.cavalaro@hotmail.com>
2021-01-17 11:02:56 -0300

    Criação da interface repository

---
+++ blogJuliana-1/src/main/java/org/generation/blogJuliana/repository/PostagemRepository.java
@@ -0,0 +1,14 @@
+package org.generation.blogJuliana.repository;
+
+import java.util.List;
+
+import org.generation.blogJuliana.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//Indica que a classe é um repositório
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem,Long> {
+	//metodo FindAllByTituloContaingIgnoreCase
+	public List<Postagem> FindAllByTituloContaingIgnoreCase (String titulo);
+}
