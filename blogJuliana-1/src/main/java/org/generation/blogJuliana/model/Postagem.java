9a6caa8b798353074918851b2d9bd89f187664f1
Juliana Cavalaro <juliana.cavalaro@hotmail.com>
2021-01-16 21:25:23 -0300

    Criar um projeto spring

---
+++ blogJuliana-1/src/main/java/org/generation/blogJuliana/model/Postagem.java
@@ -0,0 +1,5 @@
+package org.generation.blogJuliana.model;
+
+public class Postagem {
+
+}
---
+++ blogJuliana-1/src/test/java/org/generation/blogJuliana/BlogJuliana1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.blogJuliana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogJuliana1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
