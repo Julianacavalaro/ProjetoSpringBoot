63fede1039eac0939f22b82f8525d0cb9e2e8cc1
Juliana Cavalaro <juliana.cavalaro@hotmail.com>
2021-01-16 22:15:43 -0300

    Criação da Model

--- blogJuliana-1/src/main/java/org/generation/blogJuliana/model/Postagem.java
+++ blogJuliana-1/src/main/java/org/generation/blogJuliana/model/Postagem.java
@@ -1,5 +1,75 @@
 package org.generation.blogJuliana.model;
 
-public class Postagem {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+//Essa classe é uma entidade do jpa hibernate
+@Entity
+
+//Tabela
+@Table(name = "postagem")
+public class Postagem {
+	
+	@Id
+	//Transformar o atributo em uma chave primária
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	//Nenhum título pode ficar vazio
+	
+	@NotNull
+	@Size(min = 1, max = 100)
+	private String titulo;
+	
+	@NotNull
+	@Size(min = 1, max = 500)
+	private String texto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new java.sql.Date(System.currentTimeMillis());
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	
 }
+
